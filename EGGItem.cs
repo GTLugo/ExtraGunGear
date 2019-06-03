d4729e36f57a871f83dc9c6320be63570ea3f13a
GTLugo <g.alexander.lugo@gmail.com>
2019-06-02 20:10:58 -0400

    Hotfix

--- EGGItem.cs
+++ EGGItem.cs
@@ -21,7 +21,11 @@ namespace ExtraGunGear
             }
             else if (item.ranged && !player.GetModPlayer<EGGPlayer>(mod).hasStock && player.altFunctionUse != 2)
             {
+                byte prefix = item.prefix;
+                bool fav = item.favorited;
                 item.CloneDefaults(item.type);
+                item.favorited = fav;
+                item.prefix = prefix;
                 if (item.modItem != null)
                 {
                     item.modItem.CanUseItem(player);
